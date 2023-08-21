c508c3a2e3492d2c2790d56b27128bed3b793354
eliaselherrera96 <eliaselherrera90@gmail.com>
2023-08-21 17:53:31 +0200

    change loading to wait

--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -3,7 +3,7 @@ import PostDetails from '@/app/components/postDetails';
 
 const PostDetailsPage = async ({params}) => {
     const postId = params.postId
-    const loadingJSX = (<div><h1>Loading...</h1></div>)
+    const loadingJSX = (<div><h1>wait...</h1></div>)
   return (
     <div style={{padding: "20px"}}>
         <h1>Post Details</h1>
